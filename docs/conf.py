df8aa3a56a988600dfe647a77c81cbeded24821d
Li <leochen1990@gmail.com>
2021-05-04 12:48:28 +0200

    Bump version to 0.1.2rc1

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ import sphinx_compas_theme
 project = 'Form Finder'
 copyright = 'Block Research Group - ETH Zurich'
 author = 'Tom Van Mele'
-release = '0.1.2rc0'
+release = '0.1.2rc1'
 
 version = '.'.join(release.split('.')[0:2])
 
