486499dfcf2125a823564472d88361090963b608
Li <leochen1990@gmail.com>
2021-05-04 10:12:08 +0200

    update doc conf

--- docs/conf.py
+++ docs/conf.py
@@ -2,63 +2,66 @@
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = "1.0"
+# needs_sphinx = '1.0'
 
-import sys
-import os
+from sphinx.ext.napoleon.docstring import NumpyDocstring
 
 import sphinx_compas_theme
 
-
 # -- General configuration ------------------------------------------------
 
-project = "COMPAS FormFinder"
-copyright = "Block Research Group - ETH Zurich"
-author = "tom van mele"
-release = "0.1.0"
-version = ".".join(release.split(".")[0:2])
+project = 'Form Finder'
+copyright = 'Block Research Group - ETH Zurich'
+author = 'Tom Van Mele'
+release = '0.1.0'
 
-master_doc = "index"
-source_suffix = [".rst", ]
-templates_path = ["_templates", ]
+version = '.'.join(release.split('.')[0:2])
+
+master_doc = 'index'
+source_suffix = ['.rst', ]
+templates_path = ['_templates']
 exclude_patterns = []
 
-pygments_style   = "sphinx"
-show_authors     = True
+pygments_style = 'sphinx'
+show_authors = True
 add_module_names = True
-language         = None
+language = None
 
 
 # -- Extension configuration ------------------------------------------------
 
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.doctest",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.napoleon",
-    "matplotlib.sphinxext.plot_directive",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'matplotlib.sphinxext.plot_directive',
 ]
 
 # autodoc options
 
-autodoc_default_flags = [
-    "undoc-members",
-    "show-inheritance",
-]
+autodoc_mock_imports = ["Rhino", "System", "scriptcontext", "rhinoscriptsyntax", "clr", "bpy"]
 
-autodoc_member_order = "alphabetical"
+autodoc_default_options = {
+    'undoc-members': True,
+    'show-inheritance': True,
+}
+
+autodoc_member_order = 'groupwise'
 
 autoclass_content = "class"
 
 # autosummary options
 
 autosummary_generate = True
+autosummary_mock_imports = ["Rhino", "System", "scriptcontext", "rhinoscriptsyntax", "clr", "bpy"]
 
 # napoleon options
 
-napoleon_google_docstring = True
+napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = False
 napoleon_include_private_with_doc = False
@@ -69,6 +72,40 @@ napoleon_use_admonition_for_references = False
 napoleon_use_ivar = False
 napoleon_use_param = False
 napoleon_use_rtype = False
+
+
+# first, we define new methods for any new sections and add them to the class
+def parse_keys_section(self, section):
+    return self._format_fields('Keys', self._consume_fields())
+
+
+NumpyDocstring._parse_keys_section = parse_keys_section
+
+
+def parse_attributes_section(self, section):
+    return self._format_fields('Attributes', self._consume_fields())
+
+
+NumpyDocstring._parse_attributes_section = parse_attributes_section
+
+
+def parse_class_attributes_section(self, section):
+    return self._format_fields('Class Attributes', self._consume_fields())
+
+
+NumpyDocstring._parse_class_attributes_section = parse_class_attributes_section
+
+
+# we now patch the parse method to guarantee that the the above methods are
+# assigned to the _section dict
+def patched_parse(self):
+    self._sections['keys'] = self._parse_keys_section
+    self._sections['class attributes'] = self._parse_class_attributes_section
+    self._unpatched_parse()
+
+
+NumpyDocstring._unpatched_parse = NumpyDocstring._parse
+NumpyDocstring._parse = patched_parse
 
 # plot options
 
@@ -87,28 +124,31 @@ plot_html_show_formats = False
 # intersphinx options
 
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/", None),
-    "compas": ("https://compas-dev.github.io/main", "https://compas-dev.github.io/main/objects.inv"),
+    'python': ('https://docs.python.org/', None),
+    'compas': ('https://compas-dev.github.io/compas', 'https://compas-dev.github.io/compas/objects.inv'),
 }
 
 
 # -- Options for HTML output ----------------------------------------------
 
-html_theme = "compaspkg"
+html_theme = 'compaspkg'
 html_theme_path = sphinx_compas_theme.get_html_theme_path()
-
 html_theme_options = {
-    "package_name"    : "compas_formfinder",
-    "package_title"   : project,
-    "package_version" : release,
+    "package_name": "FormFinder",
+    "package_title": project,
+    "package_version": release,
+    "package_author": "Tom Van Mele",
+    "package_description": "COMPAS package for Algebraic Graph Statics",
+    "package_repo": "https://github.com/BlockResearchGroup/FormFinder",
+    "package_docs": "https://blockresearchgroup.github.io/FormFinder/",
+    "package_old_versions_txt": "https://blockresearchgroup.github.io/FormFinder/doc_versions.txt"
 }
-
 html_context = {}
 html_static_path = []
-html_extra_path = [".nojekyll"]
-html_last_updated_fmt = ""
+html_extra_path = ['.nojekyll']
+html_last_updated_fmt = ''
 html_copy_source = False
 html_show_sourcelink = False
-html_add_permalinks = ""
+html_add_permalinks = ''
 html_experimental_html5_writer = True
 html_compact_lists = True
