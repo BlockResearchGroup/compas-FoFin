ad4a3c23ce039fafc8550db21d13431c6a223a44
Li <leochen1990@gmail.com>
2021-06-01 09:55:51 +0200

    init test

---
+++ src/compas_formfinder/ui/Rhino/FormFinder/dev/FFinit_cmd.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import compas_formfinder
+
+
+def RunCommand(is_interactive):
+    print("Init with Form Finder ", compas_formfinder.__version__)
+
+
+# ==============================================================================
+# Main
+# ==============================================================================
+
+if __name__ == '__main__':
+
+    RunCommand(True)
