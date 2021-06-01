24c53fd2636ba4350f87251fda35627438e99970
Li <leochen1990@gmail.com>
2021-06-01 10:02:07 +0200

    lint

--- src/compas_formfinder/rhino/objects/cablenetobject.py
+++ src/compas_formfinder/rhino/objects/cablenetobject.py
@@ -2,13 +2,12 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from compas.geometry import Point
-from compas.geometry import Scale
-from compas.geometry import Translation
-from compas.geometry import Rotation
-from compas.utilities import i_to_rgb
-
-import compas_rhino
+# from compas.geometry import Point
+# from compas.geometry import Scale
+# from compas.geometry import Translation
+# from compas.geometry import Rotation
+# from compas.utilities import i_to_rgb
+# import compas_rhino
 
 from .meshobject import MeshObject
 
