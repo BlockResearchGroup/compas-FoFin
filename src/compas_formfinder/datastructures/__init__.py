40a0f3456d928caf28b2591ba313bd2b28608d6e
lottilotte <56078765+lottilotte@users.noreply.github.com>
2021-06-04 10:21:38 +0200

    add cablemesh.py and meshimixin.py

---
+++ src/compas_formfinder/datastructures/__init__.py
@@ -0,0 +1,28 @@
+"""
+********************************************************************************
+compas_formfinder.datastructures
+********************************************************************************
+
+.. currentmodule:: compas_formfinder.datastructures
+
+Classes
+========
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    CableMesh
+
+"""
+
+
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+from .meshmixin import MeshMixin  # noqa: F401
+from .cablemesh import CableMesh  # noqa: F401
+
+
+__all__ = [name for name in dir() if not name.startswith('_')]
