27c53a53d8d3c5ca85af10edab5780e575f60cb3
Lotte <lottealdinger@gmail.com>
2021-12-14 15:38:05 +0100

    clean out frame

--- src/compas_fofin/ui/Rhino/FoFin/dev/FFcablemesh_move_nodes_cmd.py
+++ src/compas_fofin/ui/Rhino/FoFin/dev/FFcablemesh_move_nodes_cmd.py
@@ -2,8 +2,6 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from compas.geometry import Frame
-
 import compas_rhino
 
 from compas_fofin.rhino import get_scene
