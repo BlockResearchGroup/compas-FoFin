ed159581373c1d97eaf4eeb1494e1a560db7f3bb
brgcode <van.mele@arch.ethz.ch>
2022-03-17 00:34:50 +0100

    simplify (more needed)

--- src/compas_fofin/datastructures/cablemesh.py
+++ src/compas_fofin/datastructures/cablemesh.py
@@ -2,10 +2,10 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from compas_fd.datastructures import CableMesh as BaseCableMesh
+from compas_fd.datastructures import CableMesh
 
 
-class CableMesh(BaseCableMesh):
+class CableMesh(CableMesh):
     """The FoFin CableMesh.
     """
 
--- src/compas_fofin/rhino/__init__.py
+++ src/compas_fofin/rhino/__init__.py
@@ -2,26 +2,19 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from .forms import SettingsForm  # noqa: F401
-from .forms import Browser  # noqa: F401
-from .forms import AttributesForm  # noqa: F401
-from .forms import FF_error  # noqa: F401
-from .forms import ModifyAttributesForm  # noqa: F401
-from .forms import MenuForm
+# from .conduits import ReactionConduit
+# from .conduits import LoadConduit
+# from .conduits import PipeConduit
 
-from .helpers import select_filepath_open  # noqa: F401
-from .helpers import select_filepath_save  # noqa: F401
-from .helpers import select_vertices  # noqa: F401
-from .helpers import select_edges  # noqa: F401
-from .helpers import select_faces  # noqa: F401
-from .helpers import FF_undo  # noqa: F401
-from .helpers import get_scene  # noqa: F401
+# from .artists import CableMeshArtist
 
-from .conduits import ReactionConduit  # noqa: F401
-from .conduits import LoadConduit  # noqa: F401
-from .conduits import PipeConduit  # noqa: F401
+# from .objects import MeshObject  # noqa: F401
+# from .objects import CableMeshObject  # noqa: F401
 
-from .artists import CableMeshArtist  # noqa: F401
+# __all__ = [
+#     'ReactionConduit',
+#     'LoadConduit',
+#     'PipeConduit',
+#     'CableMeshArtist',
 
-from .objects import MeshObject  # noqa: F401
-from .objects import CableMeshObject  # noqa: F401
+# ]
