560889e4d7d8e1ebf14b3f34bdb58023e8a5c830
brgcode <van.mele@arch.ethz.ch>
2022-03-24 00:05:23 +0100

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_fofin",
-    version='0.2.1',
+    version='0.2.2',
     description="Form Finding bundle for COMPAS",
     long_description=long_description,
     long_description_content_type="text/markdown",
