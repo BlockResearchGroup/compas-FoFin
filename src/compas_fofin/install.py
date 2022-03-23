6f2befd47ccb7b399f5fe5fe3cbb46fb58207096
brgcode <van.mele@arch.ethz.ch>
2022-03-23 23:21:58 +0100

    temp solution for installer until new compas release

--- src/compas_fofin/install.py
+++ src/compas_fofin/install.py
@@ -1,20 +1,56 @@
 import os
+import sys
+import imp
+from shutil import copyfile
+from subprocess import call
+
+import compas
+import compas_rhino
+from compas_rhino.install import install as install_packages
+from compas_rhino.install_plugin import install_plugin
 
 HERE = os.path.dirname(__file__)
 UI = os.path.join(HERE, 'ui')
 
 
-def install(environment='Rhino', version='7.0'):
-    if environment != 'Rhino':
-        raise NotImplementedError
+def install(version='7.0'):
+    plugin = os.path.join(UI, 'Rhino', 'FoFin')
 
-    path = os.path.join(UI, environment, 'FoFin')
-    devpath = os.path.join(path 'dev')
-    # from devpath import __plugin__
-    # load required packages
-    # run rhino install command with required packages
-    # run rhino install plugin command with path
-    # on windows
-    # compile rui folder
-    # copy rui folder into rhino default folder
-    # report
+    if not os.path.isdir(plugin):
+        raise Exception('Cannot find the plugin: {}'.format(plugin))
+
+    plugin_dir = os.path.abspath(plugin)
+
+    # this is a simplified temp duplicate of install_plugin
+
+    plugin_path, plugin_name = os.path.split(plugin_dir)
+    plugin_path = plugin_path or os.getcwd()
+
+    plugin_dev = os.path.join(plugin_dir, 'dev')
+    plugin_info = os.path.join(plugin_dev, '__plugin__.py')
+
+    __plugin__ = imp.load_source('__plugin__', plugin_info)
+
+    if hasattr(__plugin__, 'packages'):
+        install_packages(version=version, packages=__plugin__.packages)
+
+    install_plugin(plugin)
+
+    if compas.WINDOWS:
+        ruipy = os.path.join(plugin_dev, 'rui.py')
+        ruiname = '{}.rui'.format(plugin_name)
+        python_plugins_path = compas_rhino._get_python_plugins_path(version)
+
+        call(sys.executable + " " + ruipy, shell=True)
+        copyfile(os.path.join(plugin_dev, ruiname), os.path.join(python_plugins_path, '..', '..', 'UI', ruiname))
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='FoFin UI installer command line utility.')
+
+    parser.add_argument('--version', default='7.0', choices=['6.0', '7.0'], help="Version of Rhino.")
+    args = parser.parse_args()
+
+    install(version=args.version)
---
+++ src/compas_fofin/ui/Rhino/FoFin/dev/FF_cablemesh_data_save_cmd.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import compas
+from compas_ui.app import App
+
+
+__commandname__ = 'FF_cablemesh_data_save'
+
+
+@App.error()
+def RunCommand(is_interactive):
+
+    app = App()
+
+    result = app.scene.get(name='CableMesh')
+    if not result:
+        raise Exception('There is no cablemesh in the scene.')
+
+    cablemesh = result[0]
+    mesh = cablemesh.mesh
+
+    path = app.pick_file_save('FoFin.data')
+    compas.json_dump(mesh, path)
+
+
+if __name__ == '__main__':
+    RunCommand(True)
