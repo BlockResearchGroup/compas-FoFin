e24b6de6b2ad14d85343a047e9bcf534c5fa5edb
brgcode <van.mele@arch.ethz.ch>
2022-03-23 23:22:57 +0100

    simplified node constraints

--- src/compas_fofin/ui/Rhino/FoFin/dev/FF_cablemesh_constrain_nodes_cmd.py
+++ src/compas_fofin/ui/Rhino/FoFin/dev/FF_cablemesh_constrain_nodes_cmd.py
@@ -2,10 +2,6 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from compas.geometry import Vector
-from compas.geometry import Plane
-from compas.geometry import Frame
-
 import compas_rhino
 from compas_rhino.geometry import RhinoLine
 from compas_rhino.geometry import RhinoCurve
@@ -34,65 +30,18 @@ def RunCommand(is_interactive):
     if not vertices:
         return
 
-    ctypes = ["Direction", "Line", "Plane", "Curve", "Surface"]
+    ctypes = ["Line", "Curve", "Surface"]
     ctype = app.get_string("Node constraint type?", options=ctypes)
     if not ctype:
         return
 
-    if ctype == "Direction":
-        cdirs = ["X", "Y", "Z", "XY", "YZ", "ZX"]
-        cdir = app.get_string("Constraint direction?", options=cdirs)
-        if not cdir:
-            return
-
-        if cdir in ["X", "Y", "Z"]:
-
-            if cdir == "X":
-                vector = Vector.Xaxis()
-            elif cdir == "Y":
-                vector = Vector.Yaxis()
-            elif cdir == "Z":
-                vector = Vector.Zaxis()
-
-            constraint = Constraint(vector)
-            mesh.vertices_attribute('constraint', constraint, keys=vertices)
-
-        elif cdir in ["XY", "YZ", "ZX"]:
-
-            if cdir == "XY":
-                vectors = Vector.Xaxis(), Vector.Yaxis()
-            elif cdir == "YZ":
-                vectors = Vector.Yaxis(), Vector.Zaxis()
-            elif cdir == "ZX":
-                vectors = Vector.Zaxis(), Vector.Xaxis()
-
-            for vertex in vertices:
-                origin = mesh.vertex_attributes(vertex, 'xyz')
-                frame = Frame(origin, *vectors)
-                constraint = Constraint(frame)
-                mesh.vertices_attribute('constraint', constraint, keys=vertices)
-
-    elif ctype == "Line":
+    if ctype == "Line":
         guid = compas_rhino.select_line(message="Select line constraint")
         if not guid:
             return
 
         line = RhinoLine.from_guid(guid).to_compas()
         constraint = Constraint(line)
-        for vertex in vertices:
-            constraint.location = mesh.vertex_attributes(vertex, 'xyz')
-            constraint.project()
-            mesh.vertex_attributes(vertex, 'xyz', constraint.location)
-            mesh.vertex_attribute(vertex, 'constraint', constraint)
-
-    elif ctype == "Plane":
-        guid = compas_rhino.select_line(message="Select plane constraint")
-        if not guid:
-            return
-
-        line = RhinoLine.from_guid(guid).to_compas()
-        plane = Plane(line.start, line.vector)
-        constraint = Constraint(plane)
         for vertex in vertices:
             constraint.location = mesh.vertex_attributes(vertex, 'xyz')
             constraint.project()
