11998dfbf0818acc2be5df96ce96bd3704558e26
brgcode <tom.v.mele@gmail.com>
2022-08-30 18:52:56 +0200

    black

--- src/compas_fofin/__init__.py
+++ src/compas_fofin/__init__.py
@@ -15,6 +15,7 @@ compas_fofin
 from __future__ import print_function
 
 import os
+
 # import compas
 
 
@@ -22,7 +23,7 @@ __author__ = ["tom van mele"]
 __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "MIT License"
 __email__ = "van.mele@arch.ethz.ch"
-__version__ = '0.2.2'
+__version__ = "0.2.2"
 
 
 HERE = os.path.dirname(__file__)
@@ -36,7 +37,7 @@ TEMP = os.path.abspath(os.path.join(HOME, "temp"))
 __all__ = ["HOME", "DATA", "DOCS", "TEMP"]
 
 __all_plugins__ = [
-    'compas_fofin.rhino.artists',
-    'compas_fofin.rhino.install',
-    'compas_fofin.rhino.objects',
+    "compas_fofin.rhino.install",
+    "compas_fofin.rhino.artists",
+    "compas_fofin.rhino.objects",
 ]
