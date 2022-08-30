a2252c629d4a48f8bae035e941ed206a0fbd9787
brgcode <tom.v.mele@gmail.com>
2022-08-30 18:54:03 +0200

    rhino implementation of line constraint object

--- src/compas_fofin/rhino/objects/__init__.py
+++ src/compas_fofin/rhino/objects/__init__.py
@@ -3,18 +3,26 @@ from __future__ import absolute_import
 from __future__ import division
 
 from compas.plugins import plugin
+
+from compas_fd.constraints import LineConstraint
 from compas_fofin.datastructures import CableMesh
+
 from compas_ui.rhino.objects import RhinoObject
 
 from .cablemeshobject import RhinoCableMeshObject
+from .lineconstraintobject import RhinoLineConstraintObject
 
 
 @plugin(category="ui", requires=["Rhino"])
 def register_objects():
+
+    RhinoObject.register(LineConstraint, RhinoLineConstraintObject, context="Rhino")
     RhinoObject.register(CableMesh, RhinoCableMeshObject, context="Rhino")
-    print("Rhino FoFin objects registered.")
+
+    print("FoFin Rhino Objects registered.")
 
 
 __all__ = [
-    'RhinoCableMeshObject'
+    "RhinoCableMeshObject",
+    "RhinoLineConstraintObject",
 ]
