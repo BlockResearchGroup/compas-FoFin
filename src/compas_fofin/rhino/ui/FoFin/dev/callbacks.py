1c6ed6d3e9e95e08aa41218986f095a266c576ca
brgcode <tom.v.mele@gmail.com>
2022-09-05 08:12:07 +0200

    prep for auto update on node movement

--- src/compas_fofin/rhino/ui/FoFin/dev/callbacks.py
+++ src/compas_fofin/rhino/ui/FoFin/dev/callbacks.py
@@ -3,15 +3,31 @@ from compas_fofin.objects import CableMeshObject
 
 
 def FF_on_object_update(*args):
+    """
+    Callback for the "ReplaceRhinoObject" event.
+
+    Parameters
+    ----------
+    *args : tuple
+
+    Returns
+    -------
+    None
+
+    """
     ui = UI()
 
     e = args[1]
     obj = e.NewRhinoObject
 
+    # - Update geometry (when boundary nodes are moved)
+    # - Update constraints (when constraint objects are modified)
+
     cablemesh = ui.scene.active_object
     if not isinstance(cablemesh, CableMeshObject):
         return
 
+    # only trigger as redraw if there is something to redraw
     is_dirty = False
 
     for vertex in cablemesh.mesh.vertices_where(is_anchor=True):
@@ -27,6 +43,8 @@ def FF_on_object_update(*args):
         # mark the cablemesh for redrawing
         is_dirty = True
 
+    # if the active cablemesh was affected by the Rhino object changes
+    # the scene should be redrawn
     if is_dirty:
         cablemesh.is_valid = False
         ui.scene.update()
