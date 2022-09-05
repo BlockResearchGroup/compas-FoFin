50496ba7b6ed1a6e4fca858fff7cde732f22c133
brgcode <tom.v.mele@gmail.com>
2022-09-05 22:34:56 +0200

    add option to provide value directly and allow for negative values in interactive mode

--- src/compas_fofin/rhino/ui/FoFin/dev/FF_cablemesh_scale_q_cmd.py
+++ src/compas_fofin/rhino/ui/FoFin/dev/FF_cablemesh_scale_q_cmd.py
@@ -21,85 +21,98 @@ def RunCommand(is_interactive):
     if not isinstance(cablemesh, CableMeshObject):
         raise Exception("The active object is not a CableMesh.")
 
+    options = ["Value", "Interactive"]
+    mode = ui.get_string(message="Scaling mode?", options=options)
+    if not mode:
+        return
+
     selected = ui.controller.mesh_select_edges(cablemesh)
     if not selected:
         return
 
     Q = cablemesh.mesh.edges_attribute("q", keys=selected)
 
-    # this should be a cloud call with cached values
-    # anchors, edges, loads, ... are constant
-    # only Q needs to be sent
-    # and new (free) vertex locations should be received
-    fd = ui.proxy.function("compas_fd.fd.mesh_fd_constrained_numpy")
+    if mode == "Value":
+        scale = ui.get_real("Scaling factor?", minval=-1e2, maxval=+1e2, default=1.0)
 
-    # start the dynamic scaling process
+    elif mode == "Interactive":
+        # this should be a cloud call with cached values
+        # anchors, edges, loads, ... are constant
+        # only Q needs to be sent
+        # and new (free) vertex locations should be received
+        fd = ui.proxy.function("compas_fd.fd.mesh_fd_constrained_numpy")
 
-    cablemesh.is_valid = False
-    ui.scene.update()
+        # start the dynamic scaling process
 
-    gp = Rhino.Input.Custom.GetPoint()
-    gp.SetCommandPrompt("Base point for scaling.")
+        cablemesh.is_valid = False
+        ui.scene.update()
 
-    gp.Get()
-    if gp.CommandResult() != Rhino.Commands.Result.Success:
-        return False
+        gp = Rhino.Input.Custom.GetPoint()
+        gp.SetCommandPrompt("Base point for scaling.")
 
-    o = gp.Point()
+        gp.Get()
+        if gp.CommandResult() != Rhino.Commands.Result.Success:
+            return False
 
-    gp.SetCommandPrompt("Reference 1.")
-    gp.SetBasePoint(o, False)
-    gp.DrawLineFromPoint(o, True)
+        o = gp.Point()
 
-    gp.Get()
-    if gp.CommandResult() != Rhino.Commands.Result.Success:
-        return False
+        gp.SetCommandPrompt("Reference point 1.")
+        gp.SetBasePoint(o, False)
+        gp.DrawLineFromPoint(o, True)
 
-    r1 = gp.Point()
-    v1 = r1 - o
-    l1 = v1.SquareLength
+        gp.Get()
+        if gp.CommandResult() != Rhino.Commands.Result.Success:
+            return False
 
-    def OnDynamicDraw(sender, e):
-        cablemesh.clear_conduits()
+        r1 = gp.Point()
+        v1 = r1 - o
+        l1 = v1.SquareLength
 
-        r2 = e.CurrentPoint
+        def OnDynamicDraw(sender, e):
+            cablemesh.clear_conduits()
+
+            r2 = e.CurrentPoint
+            v2 = r2 - o
+            l2 = v2.SquareLength
+
+            sign = +1 if Rhino.Geometry.Vector3d.Multiply(v1, v2) > 0 else -1
+            scale = sign * l2 / l1
+
+            for edge, q in zip(selected, Q):
+                cablemesh.mesh.edge_attribute(edge, "q", q * scale)
+
+            # replace this by a call with cached values
+            # cached: anchors, edges, loads
+            # sent: q
+            # received: xyz
+            result = fd(cablemesh.mesh)
+            if result:
+                cablemesh.mesh.data = result.data
+                cablemesh.is_valid = True
+                cablemesh._draw_force_overlays()
+                cablemesh._draw_reaction_overlays()
+
+        gp.SetCommandPrompt("Reference point 2.")
+        gp.SetBasePoint(o, False)
+        gp.DrawLineFromPoint(o, True)
+        gp.Constrain(Rhino.Geometry.Line(o, r1))
+        gp.DynamicDraw += OnDynamicDraw
+
+        gp.Get()
+        if gp.CommandResult() != Rhino.Commands.Result.Success:
+            return False
+
+        r2 = gp.Point()
         v2 = r2 - o
         l2 = v2.SquareLength
-        scale = l2 / l1
 
-        for edge, q in zip(selected, Q):
-            cablemesh.mesh.edge_attribute(edge, "q", q * scale)
-
-        # replace this by a call with cached values
-        # cached: anchors, edges, loads
-        # sent: q
-        # received: xyz
-        result = fd(cablemesh.mesh)
-        if result:
-            cablemesh.mesh.data = result.data
-            cablemesh.is_valid = True
-            cablemesh._draw_force_overlays()
-            cablemesh._draw_reaction_overlays()
-
-    gp.SetCommandPrompt("Reference 2.")
-    gp.SetBasePoint(o, False)
-    gp.DrawLineFromPoint(o, True)
-    gp.Constrain(Rhino.Geometry.Line(o, r1))
-    gp.DynamicDraw += OnDynamicDraw
-
-    gp.Get()
-    if gp.CommandResult() != Rhino.Commands.Result.Success:
-        return False
-
-    r2 = gp.Point()
-    v2 = r2 - o
-    l2 = v2.SquareLength
-
-    scale = l2 / l1
+        sign = +1 if Rhino.Geometry.Vector3d.Multiply(v1, v2) > 0 else -1
+        scale = sign * l2 / l1
 
     if scale:
         for edge, q in zip(selected, Q):
             cablemesh.mesh.edge_attribute(edge, "q", q * scale)
+
         result = fd(cablemesh.mesh)
         if result:
             cablemesh.mesh.data = result.data
