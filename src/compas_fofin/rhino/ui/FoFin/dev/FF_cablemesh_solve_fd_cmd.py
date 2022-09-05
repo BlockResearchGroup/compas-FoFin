36d41b8a797f161c595519cbac9bc939fb4d1e33
brgcode <tom.v.mele@gmail.com>
2022-09-05 08:12:40 +0200

    clean up

--- src/compas_fofin/rhino/ui/FoFin/dev/FF_cablemesh_solve_fd_cmd.py
+++ src/compas_fofin/rhino/ui/FoFin/dev/FF_cablemesh_solve_fd_cmd.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
+import System
+import compas_rhino
 from compas_ui.ui import UI
 from compas_fofin.objects import CableMeshObject
 
@@ -19,18 +21,31 @@ def RunCommand(is_interactive):
     if not isinstance(cablemesh, CableMeshObject):
         raise Exception("The active object is not a CableMesh.")
 
-    mesh = cablemesh.mesh
+    for vertex in cablemesh.mesh.vertices_where(is_anchor=True):
+        constraint = cablemesh.mesh.vertex_attribute(vertex, "constraint")
+
+        if not constraint or not constraint._rhino_guid:
+            continue
+
+        result, guid = System.Guid.TryParse(constraint._rhino_guid)
+        if not result:
+            continue
+
+        obj = compas_rhino.find_object(guid)
+        if not obj:
+            continue
+
+        cablemesh.update_constraint(vertex, obj)
 
     fd = ui.proxy.function("compas_fd.fd.mesh_fd_constrained_numpy")
-
-    result = fd(mesh)
+    result = fd(cablemesh.mesh)
 
     if not result:
         # this failure should just rigger an error
         print("Force-density method equilibrium failed!")
         return
 
-    mesh.data = result.data
+    cablemesh.mesh.data = result.data
     cablemesh.is_valid = True
 
     ui.scene.update()
