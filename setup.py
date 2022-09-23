a49bdf4e9bee855eca50821a439899623d8ac1c1
brgcode <tom.v.mele@gmail.com>
2022-09-23 18:52:33 +0200

    Bump version to 0.5.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ optional_requirements = {}
 
 setup(
     name="compas_fofin",
-    version='0.4.2',
+    version='0.5.0',
     description="Form Finding bundle for COMPAS",
     long_description=long_description,
     long_description_content_type="text/markdown",
