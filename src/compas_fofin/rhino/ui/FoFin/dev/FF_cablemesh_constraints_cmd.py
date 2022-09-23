ef76d2d9fd7f320d430d7c19098f66bf4701c346
brgcode <tom.v.mele@gmail.com>
2022-09-23 15:57:51 +0200

    deal with constraints properly

--- src/compas_fofin/rhino/ui/FoFin/dev/FF_cablemesh_constraints_cmd.py
+++ src/compas_fofin/rhino/ui/FoFin/dev/FF_cablemesh_constraints_cmd.py
@@ -5,13 +5,15 @@ from __future__ import division
 import Rhino
 
 import compas_rhino
-from compas_rhino.geometry import RhinoSurface
+
+# from compas_rhino.geometry import RhinoSurface
 
 from compas_ui.ui import UI
 from compas_fofin.rhino.conversions import curveobject_to_compas
 
 from compas_fofin.rhino.objects import RhinoCableMeshObject
 from compas_fd.constraints import Constraint
+from compas.artists import Artist
 
 
 __commandname__ = "FF_cablemesh_constraints"
@@ -56,7 +58,7 @@ def RunCommand(is_interactive):
         if not vertices:
             return
 
-        guid = compas_rhino.select_curve(message="Select constraint (Curve/Surface)")
+        guid = compas_rhino.select_curve(message="Select constraint (Curve)")
         if not guid:
             return
 
@@ -65,21 +67,26 @@ def RunCommand(is_interactive):
             return
 
         if obj.ObjectType == Rhino.DocObjects.ObjectType.Curve:
-            curve = curveobject_to_compas(obj)
-            constraint = Constraint(curve)
 
-        elif obj.ObjectType == Rhino.DocObjects.ObjectType.Surface:
-            guid = compas_rhino.select_surface(message="Select surface constraint")
-            if not guid:
-                return
+            constraint = None
+            for vertex in mesh.vertices():
+                temp = mesh.vertex_attribute(vertex, "constraint")
+                if temp is not None:
+                    if temp._rhino_guid == str(guid):
+                        constraint = temp
+                        break
 
-            surface = RhinoSurface.from_guid(guid).to_compas()
-            constraint = Constraint(surface)
+            if not constraint:
+                curve = curveobject_to_compas(obj)
+                constraint = Constraint(curve)
+                artist = Artist(constraint)
+                guids = artist.draw()
+                compas_rhino.rs.HideObject(guid)
+                guid = guids[0]
+                constraint._rhino_guid = str(guid)
 
         else:
             raise NotImplementedError
-
-        constraint._rhino_guid = str(guid)
 
         for vertex in vertices:
             constraint.location = mesh.vertex_attributes(vertex, "xyz")
