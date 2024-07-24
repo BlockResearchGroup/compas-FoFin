453e952a80e69ceeec0492cd48e8f8dd46e5827f
tomvanmele <tom.v.mele@gmail.com>
2024-07-24 12:13:31 +0200

    remove main

--- src/compas_fofin/rhino/install.py
+++ src/compas_fofin/rhino/install.py
@@ -4,9 +4,3 @@ from compas.plugins import plugin
 @plugin(category="install", tryfirst=True)
 def installable_rhino_packages():
     return ["compas_fofin"]
-
-
-if __name__ == "__main__":
-
-    print("This installation procedure is deprecated.")
-    print("Use `python -m compas_rhino.install` instead.")
