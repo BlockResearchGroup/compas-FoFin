689596f514e6c4f4df52f13ee8889305f9b780f0
tomvanmele <tom.v.mele@gmail.com>
2024-08-21 00:00:44 +0200

    meta

--- src/compas_fofin/__init__.py
+++ src/compas_fofin/__init__.py
@@ -2,14 +2,19 @@ from __future__ import print_function
 
 import os
 
-# import compas
-
-
 __author__ = ["tom van mele"]
 __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "MIT License"
 __email__ = "van.mele@arch.ethz.ch"
 __version__ = "0.7.0"
+
+title = "FormFinder"
+designers = ["Juney Lee"]
+developers = ["Tom Van Mele", "Li Chen"]
+documenters = ["Petras Vestartas"]
+description = """
+"""
+website = "https://gitbook.com/blockresearchgroup/compas-Fofin"
 
 
 HERE = os.path.dirname(__file__)
@@ -25,7 +30,4 @@ __all__ = ["HOME", "DATA", "DOCS", "TEMP"]
 __all_plugins__ = [
     "compas_fofin.rhino.install",
     "compas_fofin.rhino.scene",
-    # "compas_fofin.rhino.artists",
-    # "compas_fofin.rhino.objects",
-    # "compas_fofin.rhino.register",
 ]
