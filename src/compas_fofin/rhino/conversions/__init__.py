1d7d15a6152f9e1eac93641c1de2325283157aa5
tomvanmele <tom.v.mele@gmail.com>
2024-08-21 00:00:38 +0200

    conversions

--- src/compas_fofin/rhino/conversions/__init__.py
+++ src/compas_fofin/rhino/conversions/__init__.py
@@ -2,6 +2,12 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
+from .cablemesh import box_to_cablemesh
+from .cablemesh import cylinder_to_cablemesh
 from .curves import curveobject_to_compas
 
-__all__ = ["curveobject_to_compas"]
+__all__ = [
+    "box_to_cablemesh",
+    "cylinder_to_cablemesh",
+    "curveobject_to_compas",
+]
---
+++ src/compas_fofin/rhino/conversions/cablemesh.py
@@ -0,0 +1,56 @@
+from compas.itertools import pairwise
+from compas_fofin.datastructures import CableMesh
+
+
+def box_to_cablemesh(box, k, name=None):
+    # type(...) -> CableMesh
+    mesh = CableMesh.from_shape(box)  # type: CableMesh
+    mesh = mesh.subdivided(scheme="quad", k=k)
+    mesh.name = name or "CableMesh"
+    return mesh
+
+
+def cylinder_to_cablemesh(cylinder, U, V, name=None):
+    # type(...) -> CableMesh
+    mesh = CableMesh.from_shape(cylinder, u=U)  # type: CableMesh
+    mesh.name = name or "CableMesh"
+
+    # remove top/bottom
+    for vertex in list(mesh.vertices_where(vertex_degree=U)):
+        mesh.delete_vertex(vertex)
+
+    # split for subdivison along length
+    start = None
+    for edge in mesh.edges():
+        if not mesh.is_edge_on_boundary(*edge):
+            start = edge
+            break
+
+    strip = mesh.edge_strip(start)
+    if strip[0] == strip[-1]:
+        strip[:] = strip[:-1]
+
+    splits = []
+    for u, v in strip:
+        start = mesh.vertex_point(u)
+        vector = mesh.edge_vector((u, v))
+        temp = [u]
+        w = u
+        for i in range(V - 1):
+            t = (i + 1) * 1 / V
+            point = start + vector * t
+            w = mesh.split_edge(w, v, t=0.5)
+            mesh.vertex_attributes(w, "xyz", point)
+            temp.append(w)
+        temp.append(v)
+        splits.append(temp)
+
+    faces = list(mesh.faces())
+    for face in faces:
+        mesh.delete_face(face)
+
+    for right, left in pairwise(splits + splits[0:1]):
+        for (a, b), (aa, bb) in zip(pairwise(right), pairwise(left)):
+            mesh.add_face([a, b, bb, aa])
+
+    return mesh
