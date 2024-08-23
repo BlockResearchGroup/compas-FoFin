38d45e9157516d73d89c9bd62726fc62cf4adcad
tomvanmele <tom.v.mele@gmail.com>
2024-08-23 10:46:08 +0200

    include settings in constructor

--- src/compas_fofin/scene/cablemeshobject.py
+++ src/compas_fofin/scene/cablemeshobject.py
@@ -5,6 +5,7 @@ from compas.scene.descriptors.color import ColorAttribute
 
 class CableMeshObject(MeshObject):
 
+    freecolor = ColorAttribute(default=Color.white())
     anchorcolor = ColorAttribute(default=Color.red())
     constraintcolor = ColorAttribute(default=Color.cyan())
     residualcolor = ColorAttribute(default=Color.cyan())
@@ -14,30 +15,47 @@ class CableMeshObject(MeshObject):
     compressioncolor = ColorAttribute(default=Color.blue())
     tensioncolor = ColorAttribute(default=Color.red())
 
-    def __init__(self, disjoint=True, **kwargs) -> None:
-        super().__init__(disjoint=disjoint, **kwargs)
+    def __init__(
+        self,
+        show_anchors=True,
+        show_free=False,
+        show_forces=False,
+        show_residuals=False,
+        show_reactions=False,
+        show_loads=False,
+        show_selfweight=False,
+        scale_loads=1,
+        scale_forces=1,
+        scale_residuals=1,
+        scale_selfweight=1,
+        tol_vectors=1e-3,
+        tol_pipes=1e-3,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
 
-        self.show_anchors = True
-        self.show_free = False
-        self.show_forces = False
-        self.show_residuals = False
-        self.show_reactions = False
-        self.show_loads = False
-        self.show_selfweight = False
+        self.show_anchors = show_anchors
+        self.show_free = show_free
+        self.show_forces = show_forces
+        self.show_residuals = show_residuals
+        self.show_reactions = show_reactions
+        self.show_loads = show_loads
+        self.show_selfweight = show_selfweight
 
-        self.scale_loads = 1
-        self.scale_forces = 1
-        self.scale_residuals = 1
-        self.scale_selfweight = 1
+        self.scale_loads = scale_loads
+        self.scale_forces = scale_forces
+        self.scale_residuals = scale_residuals
+        self.scale_selfweight = scale_selfweight
 
-        self.tol_vectors = 0.001
-        self.tol_pipes = 0.001
+        self.tol_vectors = tol_vectors
+        self.tol_pipes = tol_pipes
 
         self.is_valid = False
 
     @property
     def settings(self):
         settings = super().settings
+
         settings["show_anchors"] = self.show_anchors
         settings["show_free"] = self.show_free
         settings["show_forces"] = self.show_forces
@@ -54,6 +72,7 @@ class CableMeshObject(MeshObject):
         settings["tol_vectors"] = self.tol_vectors
         settings["tol_pipes"] = self.tol_pipes
 
+        settings["freecolor"] = self.freecolor
         settings["anchorcolor"] = self.anchorcolor
         settings["constraintcolor"] = self.constraintcolor
         settings["residualcolor"] = self.residualcolor
@@ -62,6 +81,7 @@ class CableMeshObject(MeshObject):
         settings["selfweightcolor"] = self.selfweightcolor
         settings["compressioncolor"] = self.compressioncolor
         settings["tensioncolor"] = self.tensioncolor
+
         return settings
 
     def select_vertices(self):
