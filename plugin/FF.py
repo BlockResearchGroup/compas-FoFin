f695562fe9b29db0b511737186d7c2f68c5c13cc
tomvanmele <tom.v.mele@gmail.com>
2024-10-03 21:39:16 +0200

    use a splash screen instead of about

--- plugin/FF.py
+++ plugin/FF.py
@@ -2,21 +2,53 @@
 # venv: formfinder
 # r: compas>=2.4, compas_dr>=0.3, compas_fd>=0.5, compas_rui>=0.2, compas_session>=0.2
 
+import pathlib
+
+import Eto.Drawing  # type: ignore
+import Eto.Forms  # type: ignore
+import Rhino  # type: ignore
+import Rhino.UI  # type: ignore
+import System  # type: ignore
+
 import compas_fofin
-from compas_rui.forms import AboutForm
+
+pluginfile = Rhino.PlugIns.PlugIn.PathFromId(System.Guid("5df3b821-822e-49e0-b24e-aebbe671c3d1"))
+shared = pathlib.Path(str(pluginfile)).parent / "shared"
+
+
+class SplashForm(Eto.Forms.Dialog[bool]):
+    def __init__(self, title, url, width=800, height=400):
+        super().__init__()
+
+        self.Title = title
+        self.Padding = Eto.Drawing.Padding(0)
+        self.Resizable = False
+        self.ClientSize = Eto.Drawing.Size(width, height)
+        self.WindowStyle = Eto.Forms.WindowStyle.NONE  # type: ignore
+
+        webview = Eto.Forms.WebView()
+        webview.Size = Eto.Drawing.Size(width, height)
+        webview.Url = System.Uri(url)
+        webview.BrowserContextMenuEnabled = False
+        webview.DocumentLoading += self.action
+
+        layout = Eto.Forms.DynamicLayout()
+        layout.BeginVertical()
+        layout.AddRow(webview)
+        layout.EndVertical()
+        self.Content = layout
+
+    def action(self, sender, e):
+        if e.Uri.Scheme == "action" and e.Uri.Host == "close":
+            self.Close()
+
+    def show(self):
+        return self.ShowModal(Rhino.UI.RhinoEtoApp.MainWindow)
 
 
 def RunCommand(is_interactive):
 
-    form = AboutForm(
-        title=compas_fofin.title,
-        description=compas_fofin.description,
-        version=compas_fofin.__version__,
-        website=compas_fofin.website,
-        copyright=compas_fofin.__copyright__,
-        license=compas_fofin.__license__,
-    )
-
+    form = SplashForm(title=compas_fofin.title, url=str(shared / "index.html"))
     form.show()
 
 
