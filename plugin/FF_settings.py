b80e98f7314876c47d955bd362c13e1b3efc00ed
tomvanmele <tom.v.mele@gmail.com>
2024-10-03 21:39:28 +0200

    add settings directly

--- plugin/FF_settings.py
+++ plugin/FF_settings.py
@@ -6,6 +6,9 @@ import compas_fofin.settings
 from compas_fofin.datastructures import CableMesh
 from compas_fofin.scene import RhinoCableMeshObject
 from compas_rui.forms import SettingsForm
+from compas_rui.values import BoolValue
+from compas_rui.values import FloatValue
+from compas_rui.values import Settings
 from compas_session.namedsession import NamedSession
 
 
@@ -14,23 +17,39 @@ def RunCommand(is_interactive):
     session = NamedSession(name="FormFinder")
     scene = session.scene()
 
-    mesh: RhinoCableMeshObject = scene.find_by_itemtype(itemtype=CableMesh)
+    SETTINGS = compas_fofin.settings.SETTINGS
 
-    if mesh:
-        if "CableMesh" in compas_fofin.settings.SETTINGS:
-            for key, value in compas_fofin.settings.SETTINGS["CablMesh"].items():
-                name = "_".join(key.split("."))
-                if hasattr(mesh, name):
-                    value.set(getattr(mesh, name))
+    meshobj: RhinoCableMeshObject = scene.find_by_itemtype(itemtype=CableMesh)
+    if meshobj:
+        SETTINGS["CableMesh"] = Settings(
+            {
+                # "show_supports": BoolValue(meshobj.show_supports),
+                # "show_free": BoolValue(meshobj.show_free),
+                "show_forces": BoolValue(meshobj.show_forces),
+                "show_residuals": BoolValue(meshobj.show_residuals),
+                "show_reactions": BoolValue(meshobj.show_reactions),
+                "show_loads": BoolValue(meshobj.show_loads),
+                "show_selfweight": BoolValue(meshobj.show_selfweight),
+                "scale_forces": FloatValue(meshobj.scale_forces),
+                "scale_residuals": FloatValue(meshobj.scale_residuals),
+                "scale_loads": FloatValue(meshobj.scale_loads),
+                "scale_selfweight": FloatValue(meshobj.scale_selfweight),
+            }
+        )
 
-    settingsmesh = SettingsForm(settings=compas_fofin.settings.SETTINGS, use_tab=True)
-    if settingsmesh.show():
+    form = SettingsForm(settings=SETTINGS, use_tab=True)
 
-        if mesh:
-            if "CableMesh" in compas_fofin.settings.SETTINGS:
-                for key, value in compas_fofin.settings.SETTINGS["CablMesh"].items():
-                    name = "_".join(key.split("."))
-                    setattr(mesh, name, value.value)
+    if form.show():
+        if meshobj:
+            meshobj.show_forces = SETTINGS["CableMesh"]["show_forces"]
+            meshobj.show_residuals = SETTINGS["CableMesh"]["show_residuals"]
+            meshobj.show_reactions = SETTINGS["CableMesh"]["show_reactions"]
+            meshobj.show_loads = SETTINGS["CableMesh"]["show_loads"]
+            meshobj.show_selfweight = SETTINGS["CableMesh"]["show_selfweight"]
+            meshobj.scale_forces = SETTINGS["CableMesh"]["scale_forces"]
+            meshobj.scale_residuals = SETTINGS["CableMesh"]["scale_residuals"]
+            meshobj.scale_loads = SETTINGS["CableMesh"]["scale_loads"]
+            meshobj.scale_selfweight = SETTINGS["CableMesh"]["scale_selfweight"]
 
     if compas_fofin.settings.SETTINGS["FormFinder"]["autosave.events"]:
         session.record(name="Update Settings")
