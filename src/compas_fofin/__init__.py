d73179738321f335affcade7da158f0cd98d6f24
tomvanmele <tom.v.mele@gmail.com>
2025-03-11 23:03:37 +0100

    Bump version to 0.15.0

--- src/compas_fofin/__init__.py
+++ src/compas_fofin/__init__.py
@@ -4,7 +4,7 @@ __author__ = ["tom van mele"]
 __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "MIT License"
 __email__ = "tom.v.mele@gmail.com"
-__version__ = "0.14.0"
+__version__ = "0.15.0"
 
 HERE = os.path.dirname(__file__)
 
