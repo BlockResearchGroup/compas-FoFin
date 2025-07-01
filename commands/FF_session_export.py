c9e2248a52fa88e512d5e602a9efcb784fea8020
tomvanmele <tom.v.mele@gmail.com>
2025-07-01 17:33:14 +0200

    simplified export

--- commands/FF_session_export.py
+++ commands/FF_session_export.py
@@ -2,9 +2,11 @@
 # venv: brg-csd
 # r: compas_fofin>=0.15.2
 
-import rhinoscriptsyntax as rs  # type: ignore
+import pathlib
 
+# import rhinoscriptsyntax as rs  # type: ignore
 import compas
+from compas.datastructures import Mesh
 from compas_fofin.scene import RhinoCableMeshObject
 from compas_fofin.session import FoFinSession
 from compas_rui.forms import FileForm
@@ -13,26 +15,35 @@ from compas_rui.forms import FileForm
 def RunCommand():
     session = FoFinSession()
 
-    meshobj: RhinoCableMeshObject = session.scene.find_by_name(name="CableMesh")
-
-    options = ["Scene", "CableMesh"]
-    option = rs.GetString(message="Export", strings=options)
-    if not option:
+    meshobj: RhinoCableMeshObject = session.find_cablemesh()
+    if not meshobj:
         return
 
-    if option == "Scene":
-        filepath = FileForm.save(session.basedir, "FormFinder-scene.json")
-        if not filepath:
-            return
+    # options = ["Scene", "CableMesh"]
+    # option = rs.GetString(message="Export", strings=options)
+    # if not option:
+    #     return
 
-        compas.json_dump(session.scene, filepath)
+    # if option == "Scene":
+    #     filepath = FileForm.save(session.basedir, "FormFinder-Scene.json")
+    #     if not filepath:
+    #         return
 
-    elif option == "CableMesh":
-        filepath = FileForm.save(session.basedir, "FormFinder-cablemesh.json")
-        if not filepath:
-            return
+    #     compas.json_dump(session.scene, filepath)
 
-        compas.json_dump(meshobj.mesh, filepath)
+    # elif option == "CableMesh":
+
+    mesh: Mesh = meshobj.mesh.copy()
+    for face in list(mesh.faces_where(_is_loaded=False)):
+        mesh.delete_face(face)
+
+    basedir = session.basedir or pathlib.Path().home()
+    basename = "FormFinder-Cablemesh.json"
+    filepath = FileForm.save(str(basedir), basename)
+    if not filepath:
+        return
+
+    compas.json_dump(mesh, filepath)
 
 
 # =============================================================================
--- src/compas_fofin/session.py
+++ src/compas_fofin/session.py
@@ -20,13 +20,13 @@ class FoFinSession(Session):
     def clear(self, clear_scene=True, clear_context=True):
         for sceneobject in self.scene.objects:
             if hasattr(sceneobject, "clear_conduits"):
-                sceneobject.clear_conduits()
+                sceneobject.clear_conduits()  # type: ignore
         self.scene.clear(clear_scene=clear_scene, clear_context=clear_context)
 
     def clear_conduits(self):
         for sceneobject in self.scene.objects:
             if hasattr(sceneobject, "clear_conduits"):
-                sceneobject.clear_conduits()
+                sceneobject.clear_conduits()  # type: ignore
 
     def confirm(self, message):
         result = rs.MessageBox(message, buttons=4 | 32 | 256 | 0, title="Confirmation")
@@ -34,3 +34,13 @@ class FoFinSession(Session):
 
     def warn(self, message):
         return rs.MessageBox(message, title="Warning")
+
+    def find_cablemesh(self, warn=True):
+        from compas_fofin.datastructures import CableMesh
+        from compas_fofin.scene import RhinoCableMeshObject
+
+        cablemesh: RhinoCableMeshObject = self.scene.find_by_itemtype(CableMesh)  # type: ignore
+        if cablemesh:
+            return cablemesh
+        if warn:
+            rs.MessageBox("There is no CableMesh.", title="Warning")
